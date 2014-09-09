980c47630c30201991c148e761924686f2b346b0
Sebastian Romano <sebasromano@gmail.com>
2014-09-09 00:24:07 -0300

    Esquema DB

---
+++ cms/app/Config/Schema/esquema.sql
@@ -0,0 +1,132 @@
+/*
+SQLyog Community v10.3 
+MySQL - 5.6.15-log : Database - destapados_v2
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `answers` */
+
+DROP TABLE IF EXISTS `answers`;
+
+CREATE TABLE `answers` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `gamer_id` int(11) DEFAULT NULL,
+  `token` varchar(40) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  `answer` int(11) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '0',
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=306 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `categories` */
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `order_by` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `gamers` */
+
+DROP TABLE IF EXISTS `gamers`;
+
+CREATE TABLE `gamers` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `fb_id` bigint(20) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `gender` varchar(100) DEFAULT NULL,
+  `locale` varchar(10) DEFAULT NULL,
+  `timezone` varchar(4) DEFAULT NULL,
+  `games` int(11) DEFAULT '0',
+  `levels` int(11) DEFAULT '0',
+  `score` int(11) DEFAULT '0',
+  `active` tinyint(1) DEFAULT '0',
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `games` */
+
+DROP TABLE IF EXISTS `games`;
+
+CREATE TABLE `games` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `token` varchar(40) DEFAULT NULL,
+  `gamer_id` bigint(20) DEFAULT NULL,
+  `data` text,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=770 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `questions` */
+
+DROP TABLE IF EXISTS `questions`;
+
+CREATE TABLE `questions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `source` varchar(255) DEFAULT NULL,
+  `answer_1` varchar(255) DEFAULT NULL,
+  `answer_2` varchar(255) DEFAULT NULL,
+  `answer_3` varchar(255) DEFAULT NULL,
+  `answer_4` varchar(255) DEFAULT NULL,
+  `answer_ok` int(4) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT '0',
+  `suggested` tinyint(1) DEFAULT '0',
+  `gamer_id` int(11) DEFAULT '0',
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=133 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(150) DEFAULT NULL,
+  `last_name` varchar(150) DEFAULT NULL,
+  `username` varchar(15) DEFAULT NULL,
+  `password` varchar(40) DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `email` varchar(255) DEFAULT NULL,
+  `avatar_file_name` varchar(255) DEFAULT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  `skype` varchar(255) DEFAULT NULL,
+  `gtalk` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `bio` text,
+  `role` enum('root','admin','guide','client') DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
